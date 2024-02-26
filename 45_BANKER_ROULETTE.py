25385c09735d8fd71901632f210eabd34e68907f
TaylorWu <137697832+taylorwu541@users.noreply.github.com>
2024-02-26 19:47:36 +0800

    Update 45_BANKER_ROULETTE.py

--- 45_BANKER_ROULETTE.py
+++ 45_BANKER_ROULETTE.py
@@ -1,11 +1,19 @@
-names_string = "Angela, Ben, Jenny, Michael, Chloe"
 names = names_string.split(", ")
 # The code above converts the input into an array seperating
 #each name in the input by a comma and space.
 # 🚨 Don't change the code above 👆
 
 import random
+
+'''
 random_name = random.randint(0,4)
 paylist = (names[random_name])
-print(random_name)
-print(paylist)
+
+print(f"{paylist} is going to buy the meal today!")
+'''
+
+name_item = len(names)
+random_name = random.randint(0,name_item-1)
+paylist = (names[random_name])
+
+print(f"{paylist} is going to buy the meal today!")
