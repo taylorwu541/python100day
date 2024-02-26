edc8c873f130f36254019c76d24803ac61137f2b
TaylorWu <137697832+taylorwu541@users.noreply.github.com>
2024-02-26 18:46:54 +0800

    Create HEADS_OR_TAILS.py

---
+++ HEADS_OR_TAILS.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+con = random.randint(0,1)
+if con == 1:
+    print("Heads")
+else:
+    print("Tails")
