aa812e9eed459ad9d12ebcad8ace8c82710104bf
TaylorWu <137697832+taylorwu541@users.noreply.github.com>
2024-03-03 23:31:43 +0800

    Update 52_AVERAGE_HEIGHT.py

--- 52_AVERAGE_HEIGHT.py
+++ 52_AVERAGE_HEIGHT.py
@@ -5,3 +5,14 @@ for n in range(0, len(student_heights)):
 # 🚨 Don't change the code above 👆
   
 # Write your code below this row 👇
+
+total_height = 0
+number_of_students = 0
+
+for one_student in student_heights :
+  total_height += one_student
+  number_of_students += 1
+average_height = round(total_height / number_of_students)
+print("total height =",total_height)
+print("number of students =",number_of_students)
+print("average height =",average_height)
